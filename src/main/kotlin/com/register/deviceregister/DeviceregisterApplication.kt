a917cafe3a3a3f4a749aeb9da72a22b45c31df62
M4R1US <baltr.marius@gmail.com>
2022-11-19 18:40:37 +0100

    Init project

---
+++ src/main/kotlin/com/register/deviceregister/DeviceregisterApplication.kt
@@ -0,0 +1,11 @@
+package com.register.deviceregister
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DeviceregisterApplication
+
+fun main(args: Array<String>) {
+	runApplication<DeviceregisterApplication>(*args)
+}
---
+++ src/test/kotlin/com/register/deviceregister/DeviceregisterApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.register.deviceregister
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DeviceregisterApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
